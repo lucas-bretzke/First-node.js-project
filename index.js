bb21dc4768c954a158f3a9a6ecfe9a20dd88eb0c
lucas bretzke <lucas.bretzke@warren.com.br>
2022-09-08 23:32:26 -0300

    ...

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //Config inicial
 const express = require('express');
 const { renderSlot } = require('vue');
+const mongoose = require('mongoose')
 const app = express();
 
 //foma de ler JSON  /= middlewares
@@ -19,15 +20,20 @@ app.get('/', (req, res) => {
 
 })
 
+//mongodb+srv://LucasBretzke:<password>@apicluster.hnv3c9p.mongodb.net/?retryWrites=true&w=majority
+
+//entregar uma porta
+const DB_USER = "lucas"
+const DB_PASSWORD = encodeURIComponent("28102003")
+
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.hnv3c9p.mongodb.net/bancodaapi?retryWrites=true&w=majority`)
+    .then(() => {
+        console.log('conectado ao mongoDB!')
+        app.listen(3000)
+    })
+    .catch((err) => console.log('DEU ERRO', err))
 
 
 
 //Entregar uma rota
 app.listen(3000)
-
-
-
-
-
-
-//entregar uma porta
